907edf740a958b46b9a9683e9de2814cf9d4a24c
justin.kasteleijn@gmail.com <justin.kasteleijn@gmail.com>
2026-04-18 20:19:58 +0200

    Added second example for my report, this heavily suffers from not using the type keyword, the Performance one less. But both are good to use for the next paper

--- GenericProgrammingAssocTypes.hs
+++ GenericProgrammingAssocTypes.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE OverloadedRecordDot #-}
 
 import Data.Foldable (fold)
@@ -16,11 +16,6 @@ instance Monoid Wattage where
   mempty = Wattage 0
 
 data EcoHome     = EcoHome
-
-data PowerSource = Solar [Wattage]
-
-instance Show PowerSource where
-  show (Solar gains) = show (fold gains)
 
 data TVMode = Performance
             | Balanced
@@ -41,20 +36,31 @@ tvPowerUsage (Television mode _)  = evalMode mode
     evalMode Balanced    = Wattage 75
     evalMode Eco         = Wattage 50
 
-class SmartHome p d h | h -> p, h -> d where
-  currentConsumption :: h -> d -> p -> Wattage
+class SmartHome h where
+  data PowerSource h
+  data Device      h
+  currentConsumption :: h -> Device h -> PowerSource h -> Wattage
 
-instance SmartHome PowerSource Television EcoHome where
-  currentConsumption :: EcoHome -> Television -> PowerSource -> Wattage
-  currentConsumption EcoHome tv (Solar gains) =
-     let (Wattage solarGain) = fold gains
-         (Wattage tvUsage) = tvPowerUsage tv
-      in Wattage (solarGain - tvUsage)
+instance SmartHome EcoHome where
+  data PowerSource EcoHome = Solar [Wattage]
+  data Device      EcoHome = TV Television
+
+  currentConsumption :: EcoHome -> Device EcoHome -> PowerSource EcoHome -> Wattage
+  currentConsumption EcoHome (TV tv) (Solar gains) =
+    let (Wattage solarGains) = fold gains
+        (Wattage tvUsage)    = tvPowerUsage tv
+     in Wattage (solarGains - tvUsage)
+
+instance Show (PowerSource EcoHome) where
+  show (Solar gains) = show (fold gains)
+
+instance Show (Device EcoHome) where
+  show (TV tv) = show tv.mode
 
 main :: IO ()
 main = do
   let myHome   = EcoHome
-  let myTv     = Television Eco True -- An Eco-friendly TV that is turned ON
+  let myTv     = TV (Television Eco True) -- An Eco-friendly TV that is turned ON
 
   -- 2. Define Solar Gains (a list of Consumption values)
   -- We use fold to sum these up in the instance
@@ -63,9 +69,10 @@ main = do
   -- 3. Calculate the current consumption
   -- This triggers the 'SmartHome' instance logic
   let balance = currentConsumption myHome myTv mySolar
+  let TV rawTv = myTv
 
   putStrLn "--- SmartHome Energy Report ---"
-  putStrLn $ "TV Mode:      " ++ show (myTv.mode)
+  putStrLn $ "TV Mode:      " ++ show (rawTv.mode)
   putStrLn $ "Solar Input:  " ++ show mySolar
-  putStrLn $ "TV Usage:     " ++ show (tvPowerUsage myTv)
+  putStrLn $ "TV Usage:     " ++ show (tvPowerUsage rawTv)
   putStrLn $ "Net Balance:  " ++ show balance
